14986e1815a8b02d340a65793a9b91e136496046
Kateryna Halan <kateryna.halan@gmail.com>
2024-02-20 16:29:58 -0500

    Added test cases for monsters reducer and service

--- src/reducers/monsters/monsters.reducer.spec.ts
+++ src/reducers/monsters/monsters.reducer.spec.ts
@@ -1,12 +1,15 @@
-import { fetchMonstersData, setSelectedMonster } from './monsters.actions';
+import { fetchMonstersData, fetchStartBattle, setSelectedMonster } from './monsters.actions';
 import { monstersReducer } from './monsters.reducer';
 import monstersData from '../../../data/monsters.json';
+import winnerData from '../../../data/winner.json';
 
 describe('Monsters Reducer', () => {
   it('should return the initial state', () => {
     expect(monstersReducer(undefined, { type: undefined })).toEqual({
+      loader: false,
       monsters: [],
       selectedMonster: null,
+      winner: null,
     });
   });
 
@@ -39,6 +42,42 @@ describe('Monsters Reducer', () => {
     expect(state).toEqual(
       expect.objectContaining({
         monsters: monstersData.monsters,
+      }),
+    );
+  });
+
+  it('should not change the winner and the loader should be changed on action pending', () => {
+    const action = { type: fetchStartBattle.pending };
+    const state = monstersReducer(undefined, action);
+    expect(state).toEqual(
+      expect.objectContaining({
+        winner: null,
+        loader: true
+      }),
+    );
+  });
+
+  it('should not change the winner and should change the loader on action rejected', () => {
+    const action = { type: fetchStartBattle.rejected };
+    const state = monstersReducer(undefined, action);
+    expect(state).toEqual(
+      expect.objectContaining({
+        winner: null,
+        loader: false
+      }),
+    );
+  });
+
+  it('should change the winner and the loader on action fulfilled', () => {
+    const action = {
+      type: fetchStartBattle.fulfilled,
+      payload: winnerData,
+    };
+    const state = monstersReducer(undefined, action);
+    expect(state).toEqual(
+      expect.objectContaining({
+        winner: winnerData.winner,
+        loader: false
       }),
     );
   });
--- src/reducers/monsters/monsters.service.spec.ts
+++ src/reducers/monsters/monsters.service.spec.ts
@@ -1,5 +1,7 @@
 import { MonsterService } from './monsters.service';
 import monstersData from '../../../data/monsters.json';
+import winnerData from '../../../data/winner.json';
+import { Battle } from '../../models/interfaces/monster.interface';
 
 describe('Monsters Service', () => {
   it('should return the monsters list empty', async () => {
@@ -17,5 +19,17 @@ describe('Monsters Service', () => {
       monstersData.monsters[0],
       monstersData.monsters[1],
     ]);
+  });
+
+  it('should return the winner', async () => {
+    jest
+      .spyOn(MonsterService, 'startBattle')
+      .mockResolvedValue(winnerData);
+    const data: Battle = {
+      monster1Id: monstersData.monsters[0].id,
+      monster2Id: monstersData.monsters[1].id
+    }
+    const response = await MonsterService.startBattle(data);
+    expect(response).toEqual(winnerData);
   });
 });
